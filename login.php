5cd81b30f8e5ba850605a31a74c5ad64ed22816f
m-camara <m.camaradwwm@gmail.com>
2025-10-01 13:56:47 +0200

    page connexion

---
+++ login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+    <div class ="fondvilla">
+        <img src="assets/villafrance.png" alt="villa en france">
+    </div>
+    <div class="logoetco">
+        <div class ="logoyokoso">
+            <img src="assets/yokoso.png" alt="logo du site">
+            <img src="assets/logoyokoso.png" alt="logo templier">
+        </div>
+        <div class="titre">
+            <h2>Se connecter</h2>
+        </div>
+    <div class="connexion">
+        <form method="POST" action="">
+
+        <div class="inputkabe">
+            <div class="cadreconnexion">
+            <div class="allinput">
+
+            <div class="email">
+            <label for="email">Email:</label>
+            <input type="text" id ="email" name="email" required>
+            </div>
+
+            <div class="mdp">
+            <label for="mdp">Mot de passe:</label>
+            <input type="password" id ="mdp" name="mdp" required>
+            </div>
+            <div class="submit">
+            <input type="submit" value="Connectez - vous" class="connexion">
+            </div>
+            </div>
+        </div>
+    </form>
+    </div>
+</body>
+</html>
