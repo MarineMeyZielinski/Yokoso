f64553ffc232ddb7193e1dd0ec98e0c4b1f2dd57
lolo435 <L.Favret.webdev@gmail.com>
2025-10-08 13:53:56 +0200

    changes page favoris

--- favoris.php
+++ favoris.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>YOKOSO - Mes favoris</title>
-    <link rel="stylesheet" href="assets/scss/main.css">
+    <link rel="stylesheet" href="assets/css/style.css">
    <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
@@ -41,23 +41,34 @@
         <button class="icon-btn" title="Profil"><i class="fa-solid fa-user" style="color: #000000;"></i></button>
       </div>
 
-<section>
+<section class="favoris-section">
         <h3 class="section-title">Mes favoris :</h3>
-        <div class="cards">
-          <article class="card">
+        <div class="cards-favoris">
+          <article class="cards">
             <img src="images/kioshi.jpg" alt="Appartement de Kioshi" class="thumb">
+            <div class="textlike">
             <div class="name">Appartement de Kioshi :</div>
             <p class="desc">Studio moderne et lumineux à deux pas du célèbre carrefour, avec lit confortable, cuisine équipée et Wi‑Fi rapide. Profitez du calme d'une rue discrète tout en étant au cœur de l'énergie tokyoïte.</p>
+            <i class="fa-solid fa-heart heart-btn active"></i>
+            </div>
           </article>
-          <article class="card">
-            <img src="images/appartement-lucas.jpg" alt="Appartement de Luca" class="thumb">
+
+          <article class="cards">
+            <img src="images/appartement-lucas.jpg" alt="Appartement de Luca" class="thumb" style="width:260px">
+            <div class="textlike">
             <div class="name">Appartement de Luca :</div>
             <p class="desc">Charmant studio au cœur de Paris, alliant confort moderne et authenticité. À deux pas des cafés typiques et des monuments emblématiques, idéal pour découvrir la Ville Lumière.</p>
+            <i class="fa-solid fa-heart heart-btn active"></i>
+            </div>
           </article>
-          <article class="card">
-            <img src="images/appartement-saitama.jpg" alt="Appartement de Saitama" class="thumb">
+
+          <article class="cards">
+            <img src="images/appartement-saitama.jpg" alt="Appartement de Saitama" class="thumb" style="height: 160px">
+            <div class="textlike">
             <div class="name">Appartement de Saitama :</div>
             <p class="desc">Maison typique au toit rouge et tatamis, entourée de verdure et proche de la mer turquoise. Une immersion authentique dans la culture d'Okinawa, entre calme et nature.</p>
+            <i class="fa-solid fa-heart heart-btn active"></i>
+            </div>
           </article>
         </div>
 </section>
